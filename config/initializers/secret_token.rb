5eda8a9f789fc416ae3f522a29262a9a459f25e6
catfriend <eakari@live.com>
2014-02-14 20:31:19 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railscast250::Application.config.secret_token = '70e243c509ecc28db7769fe02afeeab1a6e1f43bbf9c9c47fa8db987e4fcef9a403eefde61fc85839c595cb1e8d3d047f06b88b7253a6f39d2a17fd73a92737e'
